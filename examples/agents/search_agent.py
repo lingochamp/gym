713ce9bda76112c9f299beea14d1e8be75a2110f
Yuan He <lendage@gmail.com>
2016-07-08 12:09:50 +0800

    Add ability to draw a state line

--- examples/agents/search_agent.py
+++ examples/agents/search_agent.py
@@ -4,13 +4,14 @@ import numpy as np
 
 import gym
 from gym.envs.engzo.models import Knowledge, Activity, KnowledgeGroup
+from engzo_agent import BaseAgent
 
-
-class SearchAgent(object):
-    def __init__(self, actions, knowledges, simulator):
+class SearchAgent(BaseAgent):
+    def __init__(self, actions, knowledges, simulator, max_steps):
         self.__actions = actions
         self.__knowledges = knowledges
         self.__simulator = simulator
+        BaseAgent.__init__(self, max_steps)
 
     def act(self, observation):
         best_action, best_reward = None, 0.
@@ -33,19 +34,20 @@ if __name__ == "__main__":
     env = gym.make('EngzoAdaptiveLearning-v0')
     env.monitor.start('/tmp/engzo-agent-results', force=True)
 
-    agent = SearchAgent(env.activities, env.knowledges, env.simulator)
-
     max_episode, max_step = 100, 1000
+
+    agent = SearchAgent(env.activities, env.knowledges, env.simulator, max_step)
 
     for episode in range(max_episode):
         ob = env.reset()
         reward_tot = 0.
+        agent.viewer.geoms = []
 
         for step in range(max_step):
             action = agent.act(ob)
             ob, reward, done, _ = env.step(action)
             reward_tot += reward
-            print step, sum(ob)
+            agent.draw_state(step, sum(ob))
 
             if done:
                 break
@@ -53,4 +55,3 @@ if __name__ == "__main__":
         print "new state", sum(ob), reward_tot / max_step
 
     env.monitor.close()
-
